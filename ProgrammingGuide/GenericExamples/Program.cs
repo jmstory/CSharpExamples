9d69ded4d2be30f4d8c9015548bcba58891abf9c
JeonMinKi <jeonminki12@gmail.com>
2019-04-13 21:07:20 +0900

    1

---
+++ ProgrammingGuide/GenericExamples/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+  
+
+namespace GenericExamples
+{
+ 
+    class Program
+    {
+        
+        public class GenericList<T>
+        {
+            public void Add(T input ){}
+        }
+        private class ExampleClass{}
+        static void Main(string[] args)
+        {
+            GenericList<int> list1 = new GenericList<int>();
+            list1.Add(1);
+
+            GenericList<string> list2 = new GenericList<string>();
+            list2.Add("");
+
+            GenericList<ExampleClass> list3 = new GenericList<ExampleClass>();
+            list3.Add(new ExampleClass());
+        }
+    }
+}
