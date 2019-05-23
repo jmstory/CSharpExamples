729307fa28069e3e1f70188e9e78224f7f4568cc
JeonMinKi <jeonminki12@gmail.com>
2019-05-24 00:23:50 +0900

    QueryCollectionOfObject / ReturnQueryFromMethod / WriteLINQ

---
+++ LINQ/ReturnQueryFromMethod/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReturnQueryFromMethod
+{    
+    class Program
+    {
+        IEnumerable<string> QueryMethod1(ref int[] ints)
+        {
+            var intsToString = from i in ints where i > 4 select i.ToString();
+            return intsToString;
+        }
+
+        void QueryMethod2(ref int [] ints , out IEnumerable<string> returnQ)
+        {
+            var intsToString = from i in ints where i < 4 select i.ToString();
+            returnQ = intsToString;
+        }
+        static void Main(string[] args)
+        {
+            Program app = new Program();
+            int [] nums = {0,1,2,3,4,5,6,7,8,9};
+
+            var myQuery1 = app.QueryMethod1(ref nums);
+
+            foreach( string s in myQuery1)
+            {
+                 Console.WriteLine(s);
+            }
+
+            Console.WriteLine("\nResults of executing myQuery1 directly:");
+       
+            foreach (string s in app.QueryMethod1(ref nums))
+            {
+                Console.WriteLine(s);
+            }
+
+            IEnumerable<string> myQuery2;
+            app.QueryMethod2(ref nums , out myQuery2);
+
+            Console.WriteLine("\nResults of executing myQuery2:");
+            foreach (string s in myQuery2)
+            {
+                Console.WriteLine(s);
+            }
+
+            myQuery1 = from item in myQuery1 orderby item descending select item;
+
+            Console.WriteLine("\nResults of executing modified myQuery1:");
+            foreach (string s in myQuery1)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ LINQ/WriteLINQ/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WriteLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>(){5,4,1,3,9,8,6,7,2,0};
+            //Query Syntax
+            {
+                //#1
+                IEnumerable<int> filteringQuery = from num in numbers 
+                                        where num < 3 || num > 7 select num;
+
+                //#2
+                IEnumerable<int> orderingQuery = from num in numbers where num < 3 || num > 7
+                                                    orderby num ascending select num;
+
+                //#3
+                string[] groupingQuery = {"carrots", "cabbage", "brocoli", "beans", "barley"};
+                IEnumerable<IGrouping<char, string>> queryFoodGroups = from item in groupingQuery
+                                                                        group item by item[0];
+            }
+
+            List<int> numbers1 = new List<int>(){5,4,1,3,9,8,6,7,2,0};
+            List<int> numbers2 = new List<int>(){15,14,11,13,19,18,16,17,12,10};
+            //Method Syntax
+            {
+                //#4
+                double avrage = numbers1.Average();
+
+                //#5
+                IEnumerable<int> concatenationQuery = numbers1.Concat(numbers2);
+
+                //#6
+                IEnumerable<int> largeNumbersQuery = numbers2.Where(c => c > 15);
+            }     
+
+            //Mixed query and method syntax
+            {
+                //#7
+                int numCount1 = (from num in numbers1 where num < 3 || num > 7 select num).Count();
+
+                //Better
+                IEnumerable<int> numbersQuery = from num in numbers1 
+                                                    where num < 3 || num > 7 select num;   
+            
+                int numCount2 = numbersQuery.Count();
+            }
+
+            var numCount = numbers.Where(n => n < 3 || n > 7).Count();
+            int numCount3 = numbers.Where(n => n < 3 || n > 7).Count();
+
+
+            
+        }
+    }
+}
